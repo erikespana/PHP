953a0dc458a2d971a71cbd30d93a945aab03397c
Erik Espana <erikespana@users.noreply.github.com>
2017-12-28 14:24:08 -0600

    Create css-in-html.php

---
+++ search-and-replace/css-in-html.php
@@ -0,0 +1,14 @@
+// create array of patterns to match, ie: "align-right"
+$styles = ["align-right", "align-center"]
+/*
+
+foreach page in pages
+  echo page
+  foreach style in styles
+    if (preg_match (style, page.wysiwyg)
+      echo "page has style";
+    else
+      echo "0";
+  }
+ }
+*/
